6def189f230e04990d7ff27403409ea1c86635e2
Helton Gabriel Dick <heltondick123@gmail.com>
2025-10-24 18:17:50 -0300

    ta la

---
+++ Cinema/Cinema/Repository/GenerosFilmesRepository.cs
@@ -0,0 +1,99 @@
+﻿using Cinema.Data;
+using Cinema.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cinema.Repository
+{
+    public class GenerosFilmesRepository : IGenerosFilmesRepository
+    {
+        private readonly CinemaContext _context;
+        public GenerosFilmesRepository(CinemaContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Create(GenerosFilmes generosFilmes)
+        {
+            await _context.GenerosFilmes.AddAsync(generosFilmes);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(GenerosFilmes generosFilmes)
+        {
+            _context.GenerosFilmes.Remove(generosFilmes);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<GenerosFilmes>? Get(int generoId, int filmeId)
+        {
+            var generosFilmes = await _context.GenerosFilmes
+                .Include(gf => gf.Genero)
+                .Include(gf => gf.Filme)
+                .Where(gf => gf.GeneroId == generoId && gf.FilmeId == filmeId)
+                .FirstOrDefaultAsync();
+
+            return generosFilmes;
+        }
+
+        public async Task<List<GenerosFilmes>> GetAll()
+        {
+            var generosFilmes = await _context.GenerosFilmes
+                .Include(gf => gf.Genero)
+                .Include(gf => gf.Filme)
+                .ToListAsync();
+            return generosFilmes;
+        }
+
+        public async Task<List<GenerosFilmes>?> GetByFilmeId(int filmeId)
+        {
+            var generosFilmes = await _context.GenerosFilmes
+                .Include(gf => gf.Genero)
+                .Include(gf => gf.Filme)
+                .Where(gf => gf.FilmeId == filmeId)
+                .ToListAsync();
+            return generosFilmes;
+        }
+
+        public async Task<List<GenerosFilmes>> GetByFilmeName(string filmeName)
+        {
+            var generosFilmes = await _context.GenerosFilmes
+                .Include(gf => gf.Genero)
+                .Include(gf => gf.Filme)
+                .Where(gf => gf.Filme!.Titulo!
+                    .ToLower()
+                    .Contains(filmeName.ToLower())
+                )
+                .ToListAsync();
+            return generosFilmes;
+        }
+
+        public async Task<List<GenerosFilmes>?> GetByGeneroId(int generoId)
+        {
+            var generosFilmes = await _context.GenerosFilmes
+                .Include(gf => gf.Genero)
+                .Include(gf => gf.Filme)
+                .Where(gf => gf.GeneroId == generoId)
+                .ToListAsync();
+            return generosFilmes;
+        }
+
+        public Task<List<GenerosFilmes>> GetByGeneroName(string generoName)
+        {
+            var generosFilmes = _context.GenerosFilmes
+                .Include(gf => gf.Genero)
+                .Include(gf => gf.Filme)
+                .Where(gf => gf.Genero!.Name!
+                    .ToLower()
+                    .Contains(generoName.ToLower())
+                )
+                .ToListAsync();
+            return generosFilmes;
+        }
+
+        public Task Update(GenerosFilmes generosFilmes)
+        {
+            _context.GenerosFilmes.Update(generosFilmes);
+            return _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Cinema/Cinema/Repository/IGenerosFilmesRepository.cs
@@ -0,0 +1,20 @@
+﻿using Cinema.Models;
+
+namespace Cinema.Repository
+{
+    public interface IGenerosFilmesRepository
+    {
+        public Task Create(GenerosFilmes generosFilmes);
+        public Task Update(GenerosFilmes generosFilmes);
+        public Task Delete(GenerosFilmes generosFilmes);
+
+        public Task<List<GenerosFilmes>?> GetByFilmeId(int filmeId);
+        public Task<List<GenerosFilmes>?> GetByGeneroId(int generoId);
+
+        public Task<GenerosFilmes>? Get(int generoId, int filmeId);
+        public Task<List<GenerosFilmes>> GetAll();
+
+        public Task<List<GenerosFilmes>> GetByFilmeName(string filmeName);
+        public Task<List<GenerosFilmes>> GetByGeneroName(string generoName);
+    }
+}
